7a96e00e8db00098298118eef4b882967495744e
unknown <sebastianswiecz458@gmail.com>
2022-08-03 16:31:15 +0200

    Added all lifecycle hooks

--- src/api/produkt/content-types/produkt/lifecycles.js
+++ src/api/produkt/content-types/produkt/lifecycles.js
@@ -1,7 +1,31 @@
 const axios = require("axios").default;
 
 module.exports = {
+  async afterCreate(event) {
+    const { data } = event.params;
+
+    try {
+      await axios.post(process.env.REVALIDATE_URL, {
+        slugToRevalidate: data.Link,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
   async afterUpdate(event) {
+    const { data } = event.params;
+
+    try {
+      await axios.post(process.env.REVALIDATE_URL, {
+        slugToRevalidate: data.Link,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  async afterDestroy(event) {
     const { data } = event.params;
 
     try {
